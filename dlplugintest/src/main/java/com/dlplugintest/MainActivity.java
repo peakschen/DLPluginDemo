eaf90dc707591f731c243fac50c736bd1738dd90
peakschen <gaofengchen516@126.com>
2016-07-05 12:08:04 +0800

    插件开发

---
+++ dlplugintest/src/main/java/com/dlplugintest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dlplugintest;
+
+import android.os.Bundle;
+
+import com.ryg.dynamicload.DLBasePluginActivity;
+
+public class MainActivity extends DLBasePluginActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
